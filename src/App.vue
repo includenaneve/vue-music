e06c4345a27ad7bc76c08b242176c717ef14f99a
naneve <includenaneve@outlook.com>
2018-03-09 23:47:10 +0800

    init less

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <m-header></m-header>
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+ import MHeader from 'components/m-header/m-header.vue'
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="less" rel="stylesheet/less">
+  @import "common/less/variable.less";
+  #app {
+    color:@color-theme;
+  }
 </style>
