e621b5930922ea30bb1f65ce839ae0dd94ecdac4
naneve <includenaneve@outlook.com>
2018-04-09 00:04:55 +0800

    @touchStart @touchMove事件

--- src/base/listView/listView.vue
+++ src/base/listView/listView.vue
@@ -1,7 +1,7 @@
 <template>
-  <scroll class="listView" :data="data">
+  <scroll class="listView" :data="data" ref="listView">
     <ul>
-      <li v-for="(group,index) in data" class="list-group" :key="index">
+      <li v-for="(group,index) in data" class="list-group" :key="index" ref="listGroup">
         <h2 class="list-group-title">{{group.title}}</h2>
         <ul>
           <li v-for="(item,index) in group.items" class="list-group-item" :key="index">
@@ -11,17 +11,55 @@
         </ul>
       </li>
     </ul>
+    <ul class="list-shortcut">
+      <li v-for="(item,index) in shortcutList" class="item" @touchstart="onShortcutTouchStart"
+          @touchmove.stop.prevent="onShortcutTouchMove" :data-index="index" :key="index">
+        {{item}}
+      </li>
+    </ul>
   </scroll>
 </template>
 
 <script>
   import Scroll from 'base/scroll/scroll';
+  import {getData} from 'common/js/dom';
+
+  const ANCHOR_HEIGHT = 18;
 
   export default {
+    created() {
+      this.touch = {}
+    },
     props: {
       data: {
         type: Array,
         default: null
+      }
+    },
+    computed: {
+      shortcutList() {
+        return this.data.map((group) => {
+          return group.title.substr(0, 1);
+        })
+      }
+    },
+    methods: {
+      onShortcutTouchStart(e) {
+        const anchorIndex = getData(e.target, 'index');
+        let firstTouch = e.touches[0];
+        this.touch.y1 = firstTouch.pageY;
+        this.touch.anchorIndex = anchorIndex;
+        this._scrollTo(anchorIndex);
+      },
+      onShortcutTouchMove(e) {
+        let firstTouch = e.touches[0];
+        this.touch.y2 = firstTouch.pageY;
+        let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0;
+        let anchorIndex = parseInt(this.touch.anchorIndex) + delta;
+        this._scrollTo(anchorIndex);
+      },
+      _scrollTo(index) {
+        this.$refs.listView.scrollToElement(this.$refs.listGroup[index], 1000);
       }
     },
     components: {Scroll}
--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -11,3 +11,13 @@ export function hasClass(el, className) {
   let reg = new RegExp('(^|\\s)' + className + '(\\s|$)');
   return reg.test(el.className);
 }
+
+export function getData(element, name, value) {
+  const prefix = 'data-';
+  const propName = prefix + name;
+  if (value) {
+    return element.setAttribute(propName, value);
+  } else {
+    return element.getAttribute(propName);
+  }
+}
